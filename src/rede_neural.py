135ac10b02b5f0b6f88e8d5c27d7901ceaed5e24
MathausC <mathaus273@gmail.com>
2022-06-01 22:49:24 -0300

    Commitando para arthur ver

--- src/rede_neural.py
+++ src/rede_neural.py
@@ -1,5 +1,48 @@
+import src.perceptron as perceptron
 
+class rede_neural():
+    def __init__(self, camadas, num_entradas, taxa_de_aprendizagem, faixa_de_pesos) -> None:
+        self.camadas = camadas
+        self.num_entradas = num_entradas
+        self.taxa_de_aprendizagem = taxa_de_aprendizagem
+        self.faixa_de_pesos = faixa_de_pesos
+        self.rede = []
+        self.perceptron_saida = self.perceptron_factory()
+        self.resultado = 0
+        self.build()
 
-class RedeNeural():
+    def build(self) -> None:
+        for c in range(self.camadas):
+            camada = []
+            for e in range(self.num_entradas):
+                p = self.perceptron_factory()
+                camada.append(p)
+            self.rede.append(camada.copy())
     
-    pass
+    def perceptron_factory(self) -> perceptron:
+        p = perceptron.get_instace(self.num_entradas,
+            self.faixa_de_pesos,
+            self.taxa_de_aprendizagem)
+        return p
+
+    def process(self, entrada) -> None:
+        self.resultado = 0
+        camada = len(self.rede)
+        for c in range(camada):
+            perc = len(self.rede[c])
+            res =[]
+            for i in range(perc):
+                self.rede[c][i].process(entrada)
+                res.append(self.rede[c][i].get_result())
+            entrada = res.copy()
+        self.perceptron_saida.process(entrada)
+        self.resultado = self.perceptron_saida.get_result()
+    
+    def get_result(self):
+        return self.resultado
+
+    def compare_expected(self, expected):
+        
+
+def get_instance(camadas, num_entradas, taxa_de_aprendizagem, faixa_de_pesos) -> rede_neural:
+    return rede_neural(camadas, num_entradas, taxa_de_aprendizagem, faixa_de_pesos)
--- tests/test_rede_neural.py
+++ tests/test_rede_neural.py
@@ -1,13 +1,52 @@
 import unittest
 
+from src.perceptron import perceptron
 import src.rede_neural as rede_neural
 
 class test_rede_neural(unittest.TestCase):
 
     def setUp(self):
-        self.rede_neural = rede_neural.get_instance();
+        self.camadas = 2
+        self.num_estradas = 5
+        self.taxa_de_aprendizagem = 0.5
+        self.faixa_de_pesos = 1
+        self.rede_neural = rede_neural.get_instance(
+            self.camadas,
+            self.num_estradas,
+            self.taxa_de_aprendizagem,
+            self.faixa_de_pesos
+        )
 
-    
+    def test01_perceptron_factory(self):
+        per = self.rede_neural.perceptron_factory()
+        self.assertIsInstance(per, perceptron)
+
+    def test02_build_network(self):
+        rede = self.rede_neural.rede.copy()
+        self.assertEquals(len(rede), self.camadas)
+        for c in range(self.camadas):
+            self.assertEquals(len(rede[c]), self.num_estradas)
+            for i in range(self.num_estradas):
+                self.assertIsInstance(rede[c][i], perceptron)
+
+    def test03_get_result_rede_neural(self):
+        expected = 0
+        entradas = [0, 0, 0, 0, 0]
+        self.rede_neural.process(entradas)
+        res = self.rede_neural.get_result()
+        self.assertAlmostEqual(expected, res)
+
+    def test04_verify_aprentency(self):
+        expected = 3
+        entrada = [1, 1, 1, 1, 1]
+        self.rede_neural.process(entrada)
+        res1 = self.rede_neural.get_result()
+        self.rede_neural.compare_expected(expected)
+        self.rede_neural.process(entrada)
+        res2 = self.rede_neural.get_result()
+        self.assertAlmostEquals(res1, res2)
+
+
 
 if __name__ == '__main__':
     unittest.main()
